71ce0fe616f33fcb02cf253e452cbfecb0184fa7
Ricky Chen <rickychenyiqi@gmail.com>
2024-03-07 16:17:05 -0500

    Set control registers for reset and filter

--- include/driver/accelerometer.h
+++ include/driver/accelerometer.h
@@ -30,21 +30,32 @@ typedef std::pair<ErrorCode, std::string> error_t;
 #define ACCEL_READ_REG_Z_LOW 0x32
 #define ACCEL_READ_REG_Z_HIGH 0x33
 #define ACCEL_BUFFER_SIZE 6 // Reads 6 8-bit ints and stores as 3 16-bit ints
+#define ACCEL_WHO_AM_I_REG 0x0F
 
 // Control registers - LSM6DSL data sheet pg. 49
 #define ACCEL_CTRL_1_REG 0x10
+#define ACCEL_CTRL_3_REG 0x12
 #define ACCEL_CTRL_8_REG 0x17
 
 // Control Options for Ctrl 1 - LSM6DSL data sheet pg. 60
 // Bit[0:3]: ODR (Set to 6.66kHz)
 // Bit[4:5]: Sensitivity Scale (Set to 2G uncertainty)
-// Bit[6]: Linear bandwidth (Not used)
+// Bit[6]: Low Pass Filter 1's bandwidth (Ignored since LPF2 is used)
 // Bit[7]: Analog Bandwidth (Set to 1.5kHz)
 #define ACCEL_ODR_LOW_POWER 0b00000000
 #define ACCEL_ODR_6660HZ 0b10100000 // Highest performance data rate
 #define ACCEL_2G_SENSITIVITY_SCALE 0b000000
-#define ACCEL_ANALOG_BANDWIDTH 0b1
+#define ACCEL_LPF1_BW 0b00
+#define ACCEL_ANALOG_BANDWIDTH 0b0
 #define ACCEL_SENSITIVITY 0.061f // from LSM6DSL data sheet pg. 21
+
+// Control Options for Ctrl 3 - LSM6DSL data sheet pg. 62
+#define ACCEL_SW_RESET 0b00000001
+
+// Control Options for Ctrl 8 - LSM6DSL data sheet pg. 66
+#define ACCEL_LPF2_XL_EN 0b10000000 // Enable LPF2
+#define ACCEL_LPF2_CUTOFF 0b1100000 // Low-pass cutoff: ODR/400
+#define HP_SLOPE_XL_EN 0b000 // Use Low-pass
 
 /**
  * Write configuration settings to Accelerometer control register
--- src/driver/accelerometer.cpp
+++ src/driver/accelerometer.cpp
@@ -5,17 +5,53 @@
 using namespace SimpleSlam;
 
 std::optional<SimpleSlam::LSM6DSL::error_t> SimpleSlam::LSM6DSL::Accel_Init() {
+    // Wait for peripheral to turn on
+    HAL_StatusTypeDef status;
+    uint8_t who_am_i_val = 0;
+    while (who_am_i_val == 0x00) {
+        status = SimpleSlam::I2C_Mem_Read_Single(ACCEL_I2C_ADDRESS, ACCEL_WHO_AM_I_REG, &who_am_i_val);
+        RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR, std::string("Failed to read Who Am I"));
+    }
+
+    // Ctrl1 options: ODR, Sensitivity, Bandwidth
     uint8_t ctrl_1 = ACCEL_ODR_6660HZ + ACCEL_2G_SENSITIVITY_SCALE + ACCEL_ANALOG_BANDWIDTH;
-    HAL_StatusTypeDef status = I2C_Mem_Write_Single(
+    status = I2C_Mem_Write_Single(
         ACCEL_I2C_ADDRESS,
         ACCEL_CTRL_1_REG,
         ctrl_1
     );
     RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR, "Failed to write to Ctrl 1");
+
+    // Ctrl8 options: Noise Filtering
+    uint8_t ctrl_8 = ACCEL_LPF2_XL_EN + ACCEL_LPF2_CUTOFF + HP_SLOPE_XL_EN;
+    status = I2C_Mem_Write_Single(
+        ACCEL_I2C_ADDRESS,
+        ACCEL_CTRL_8_REG,
+        ctrl_8
+    );
+    RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR, "Failed to write to Ctrl 8");
     return {};
 }
 
 std::optional<SimpleSlam::LSM6DSL::error_t> SimpleSlam::LSM6DSL::Accel_DeInit() {
+    // Reset
+    HAL_StatusTypeDef status;
+    uint8_t ctrl_3 = ACCEL_SW_RESET;
+    status = I2C_Mem_Write_Single(
+        ACCEL_I2C_ADDRESS,
+        ACCEL_CTRL_3_REG,
+        ctrl_3
+    );
+    RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR, "Failed to write to Ctrl 3");
+
+    // Power Down
+    uint8_t ctrl_1 = ACCEL_ODR_LOW_POWER + ACCEL_2G_SENSITIVITY_SCALE + ACCEL_ANALOG_BANDWIDTH;
+    status = I2C_Mem_Write_Single(
+        ACCEL_I2C_ADDRESS,
+        ACCEL_CTRL_1_REG,
+        ctrl_1
+    );
+    RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR, "Failed to write to Ctrl 1");
 
     return {};
 }
