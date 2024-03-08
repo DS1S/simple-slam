4ecc5ea9cdbb99b5e1a2ed0a0dacfa5c6a436638
makandz <makan@makan.bz>
2024-03-07 19:18:13 -0500

    Base driver

---
+++ include/driver/lis3mdl.h
@@ -0,0 +1,56 @@
+/**
+ * LIS3MDL Module Driver
+ * Author: Makan Dehizadeh
+ */
+
+#pragma once
+
+#include <optional>
+#include <string>
+
+#include "driver/i2c.h"
+
+namespace SimpleSlam::LIS3MDL {
+
+// General Map
+#define LIS3MDL_I2C_DEVICE_ADDRESS 0x3C
+#define LIS3MDL_WHO_AM_I 0x0F   // h
+#define WHO_AM_I_EXPECTED 0x3D  // 61
+
+// X Y Z Map
+#define REG_X_L 0x28
+#define REG_X_H 0x29
+#define REG_Y_L 0x2A
+#define REG_Y_H 0x2B
+#define REG_Z_L 0x2C
+#define REG_Z_H 0x2D
+
+// Control Map
+#define REG_CTRL_1 0x20
+#define REG_CTRL_2 0x21
+#define REG_CTRL_3 0x22
+#define REG_CTRL_4 0x23
+#define REG_CTRL_5 0x24
+
+// Sensitivity
+#define SENSITIVITY_4G 6842
+#define SENSITIVITY_8G 3421
+#define SENSITIVITY_12G 2281
+#define SENSITIVITY_16G 1711
+
+enum class ErrorCode {
+    I2C_ERROR = 1,
+    WHO_AM_I_UNEXPECTED_VALUE = 2,
+};
+typedef std::pair<ErrorCode, std::string> error_t;
+
+typedef struct {
+    // Heads up: These use indexes, not the actual values
+    uint8_t outputRate; // 0.625, 1.25, 2.5, 5, 10, 20, 40, 80 Hz (default: 40)
+    uint8_t fullScale;  // 4, 8, 12, 16 gauss (default: 4)
+} LIS3MDL_Config_t;
+
+std::optional<error_t> Init(const LIS3MDL_Config_t& config);
+std::optional<error_t> ReadXYZ(int16_t* x, int16_t* y, int16_t* z);
+
+}  // namespace SimpleSlam::LIS3MDL
---
+++ src/driver/lis3mdl.cpp
@@ -0,0 +1,134 @@
+/**
+* LIS3MDL Module Driver
+* Author: Makan Dehizadeh
+*/
+
+#include "driver/lis3mdl.h"
+
+#define RETURN_IF_CONTAINS_ERROR(maybe_error) \
+    if (maybe_error.has_value()) {            \
+        return maybe_error;                   \
+    }
+#define RETURN_IF_STATUS_NOT_OK(status, code, message)     \
+    if (status != HAL_StatusTypeDef::HAL_OK) {             \
+        std::make_optional(std::make_pair(code, message)); \
+    }
+
+/**
+ * Initialize the LIS3MDL module
+ * @param config Configuration for the LIS3MDL module
+ */
+std::optional<SimpleSlam::LIS3MDL::error_t> SimpleSlam::LIS3MDL::Init(
+    const LIS3MDL_Config_t &config) {
+    printf("LIS3MDL::Init\n");
+
+    // Validate the who am i register
+    uint8_t who_am_i_value = 0;
+    HAL_StatusTypeDef status = I2C_Mem_Read_Single(
+        LIS3MDL_I2C_DEVICE_ADDRESS, LIS3MDL_WHO_AM_I, &who_am_i_value);
+    RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR,
+                            "Error reading who am i");
+
+    // We have an expected value for the who am i
+    if (who_am_i_value != WHO_AM_I_EXPECTED) {
+        return std::make_optional(std::make_pair(
+            ErrorCode::WHO_AM_I_UNEXPECTED_VALUE, "Unexpected who am i value"));
+    }
+
+    // Control Register 1
+    // Temp, OM1, OM0, DO2, DO1, DO0, FAST_ODR, ST
+    // Note: Not using FAST_ODR yet, not needed and unknown noise in the data
+    uint8_t regValue = 0x00;
+    uint8_t outputRate = config.outputRate;
+    regValue |= (outputRate << 2);
+    status =
+        I2C_Mem_Write_Single(LIS3MDL_I2C_DEVICE_ADDRESS, REG_CTRL_1, regValue);
+    RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR,
+                            "Error writing to control register 1");
+
+    // Control Register 2
+    // 0, FS1, FS0, 0, REBOOT, SOFT_RST, 0, 0
+    regValue = 0x00;
+    uint8_t fullScale = config.fullScale;
+    regValue |= (fullScale << 5);
+    status =
+        I2C_Mem_Write_Single(LIS3MDL_I2C_DEVICE_ADDRESS, REG_CTRL_2, regValue);
+    RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR,
+                            "Error writing to control register 2");
+
+    // Control Register 3
+    // 0, 0, LP, 0, 0, SIM, MD1, MD0
+    // Note: We aren't using the LP (low-power) mode, so we can leave it at 0
+    // Note: We aren't using the SIM (SPI mode) so we can leave it at 0
+    // Operating mode is always continuous-conversion mode (MD 00)
+    regValue = 0x00;
+    status =
+        I2C_Mem_Write_Single(LIS3MDL_I2C_DEVICE_ADDRESS, REG_CTRL_3, regValue);
+    RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR,
+                            "Error writing to control register 3");
+
+    // Control Register 4
+    // 0, 0, 0, 0, OMZ1, OMZ0, BLE, 0
+    // Note: OMZ is only for Z-axis
+    // Note: We want as much precision as possible, so we set OMZ to 11
+    // (ultra-performance) Note: We aren't using the BLE (big/little endian)
+    // mode, so we can leave it at 0
+    regValue = 0x0C;
+    status =
+        I2C_Mem_Write_Single(LIS3MDL_I2C_DEVICE_ADDRESS, REG_CTRL_4, regValue);
+    RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR,
+                            "Error writing to control register 4");
+
+    // Control Register 5
+    // FAST_READ, BDU, 0, 0, 0, 0, 0, 0
+    // Note: We aren't using the FAST_READ mode, so we can leave it at 0
+    // Note: BDU prevents reading while its writing (only writes when read)
+    regValue = 0x40;
+    status =
+        I2C_Mem_Write_Single(LIS3MDL_I2C_DEVICE_ADDRESS, REG_CTRL_5, regValue);
+    RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR,
+                            "Error writing to control register 5");
+
+    return {};
+}
+
+/**
+ * Read the X, Y, and Z values from the LIS3MDL module. Will write the values to
+ * the pointers.
+ * @param x Pointer to the X value
+ * @param y Pointer to the Y value
+ * @param z Pointer to the Z value
+ */
+std::optional<SimpleSlam::LIS3MDL::error_t> SimpleSlam::LIS3MDL::ReadXYZ(
+    int16_t *x, int16_t *y, int16_t *z) {
+    printf("LIS3MDL::ReadXYZ\n");
+
+    // The addresses are consecutive, so we can read 6 bytes in one go
+    // This chip uses low and high registers, so we need to do shifting
+    uint8_t buffer[6];
+    HAL_StatusTypeDef status =
+        I2C_Mem_Read(LIS3MDL_I2C_DEVICE_ADDRESS, REG_X_L, I2C_MEMADD_SIZE_8BIT,
+                     &buffer[0], 6);
+    RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR, "Error reading XYZ");
+
+    *x = (buffer[1] << 8) | buffer[0];
+    *y = (buffer[3] << 8) | buffer[2];
+    *z = (buffer[5] << 8) | buffer[4];
+
+    // Read the control register 2 to get the sensitivity
+    u_int8_t reg2Value = 0;
+    status = I2C_Mem_Read_Single(LIS3MDL_I2C_DEVICE_ADDRESS, REG_CTRL_2,
+                                 &reg2Value);
+    RETURN_IF_STATUS_NOT_OK(status, ErrorCode::I2C_ERROR,
+                        "Error reading control register 2");
+
+    // Now we handle the sensitivity
+    // Note: The sensitivity is in milligauss
+    int sensitivity_values[] = { SENSITIVITY_4G, SENSITIVITY_8G, SENSITIVITY_12G, SENSITIVITY_16G };
+    double sensitivity = 1000.0 / sensitivity_values[(reg2Value >> 5) & 0x03];
+    *x *= sensitivity;
+    *y *= sensitivity;
+    *z *= sensitivity;
+
+    return {};
+}
