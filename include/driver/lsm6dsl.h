5886f1f5712c63fa8600b4d41fd250fef541c76f
Aarya Patel <32047299+Aarya-Patel@users.noreply.github.com>
2024-03-14 13:51:30 -0400

    extending LSM6DSL to enable gyroscope (#7)
    * extending LSM6DSL to enable gyroscope
    
    * changing define var names

--- include/driver/lsm6dsl.h
+++ include/driver/lsm6dsl.h
@@ -24,7 +24,9 @@ typedef std::pair<ErrorCode, std::string> error_t;
             message));                                  \
     }                                                   \
 
-#define ACCEL_I2C_ADDRESS 0xD4 // from Discovery Board user manual pg. 29
+#define I2C_ADDRESS 0xD4 // from Discovery Board user manual pg. 29
+#define WHO_AM_I_REG 0x0F
+
 #define ACCEL_READ_REG_X_LOW 0x28 // from LSM6DSL data sheet pg. 49
 #define ACCEL_READ_REG_X_HIGH 0x29
 #define ACCEL_READ_REG_Y_LOW 0x30
@@ -32,12 +34,23 @@ typedef std::pair<ErrorCode, std::string> error_t;
 #define ACCEL_READ_REG_Z_LOW 0x32
 #define ACCEL_READ_REG_Z_HIGH 0x33
 #define ACCEL_BUFFER_SIZE 6 // Reads 6 8-bit ints and stores as 3 16-bit ints
-#define ACCEL_WHO_AM_I_REG 0x0F
+
+#define GYRO_READ_REG_X_LOW 0x22
+#define GYRO_READ_REG_X_HIGH 0x23
+#define GYRO_READ_REG_Y_LOW 0x24
+#define GYRO_READ_REG_Y_HIGH 0x25
+#define GYRO_READ_REG_Z_LOW 0x26
+#define GYRO_READ_REG_Z_HIGH 0x27
+#define GYRO_BUFFER_SIZE 6 // Reads 6 8-bit ints and stores as 3 16-bit ints
+
 
 // Control registers - LSM6DSL data sheet pg. 49
-#define ACCEL_CTRL_1_REG 0x10
-#define ACCEL_CTRL_3_REG 0x12
-#define ACCEL_CTRL_8_REG 0x17
+#define CTRL_1_REG 0x10
+#define CTRL_2_REG 0x11
+#define CTRL_3_REG 0x12
+#define CTRL_6_REG 0x15
+#define CTRL_7_REG 0x16
+#define CTRL_8_REG 0x17
 
 // Control Options for Ctrl 1 - LSM6DSL data sheet pg. 60
 // Bit[0:3]: ODR (Set to 6.66kHz)
@@ -50,6 +63,21 @@ typedef std::pair<ErrorCode, std::string> error_t;
 #define ACCEL_LPF1_BW 0x00
 #define ACCEL_ANALOG_BANDWIDTH 0x00
 #define ACCEL_SENSITIVITY 0.061f // from LSM6DSL data sheet pg. 21
+
+// Control Options for CTRL2-G Register - LSM6DSL data sheet pg. 61
+#define GYRO_ODR_LOW_POWER 0x00
+#define GYRO_ODR_6660HZ 0xA0
+#define GYRO_FS_G 0x00
+#define GYRO_FS_125 0x00
+#define GYRO_SENSITIVITY 8.75f
+
+// Control Options for CTRL6-G Register - LSM6DSL data sheet pg. 65
+#define GYRO_LOW_PASS_BANDWIDTH 0x03
+
+// Control Options for CTRL7-G Registers - LSM6DSL data sheet pg. 66
+#define GYRO_HIGH_PASS_EN 0x40
+#define GYRO_HIGH_PASS_BANDWIDTH 0x30
+
 
 // Control Options for Ctrl 3 - LSM6DSL data sheet pg. 62
 #define ACCEL_SW_RESET 0x01
@@ -69,6 +97,12 @@ std::optional<error_t> Accel_Init();
 std::optional<error_t> Accel_DeInit();
 
 /**
+ * Write configuration settings to Gyroscope control register
+*/
+std::optional<error_t> Gyro_Init();
+std::optional<error_t> Gyro_DeInit();
+
+/**
  * Read raw accelerometer data into buffer
 */
 std::optional<error_t> Accel_Read_Raw(int16_t* buffer);
@@ -77,5 +111,15 @@ std::optional<error_t> Accel_Read_Raw(int16_t* buffer);
  * Includes conversion with sensitivity
 */
 std::optional<error_t> Accel_Read(int16_t* buffer);
+
+/**
+ * Read raw gyroscope data into buffer
+*/
+std::optional<error_t> Gyro_Read_Raw(int16_t* buffer);
+/**
+ * Read gyroscope data (in dps) into buffer
+ * Includes conversion with sensitivity
+*/
+std::optional<error_t> Gyro_Read(int16_t* buffer);
 
 }
