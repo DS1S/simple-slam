95050db07a67da750e571dc205ae2b20116465b6
Daniel Saunders <37748500+DS1S@users.noreply.github.com>
2024-03-14 15:49:06 -0400

    Add http server (#8)
    * Add http server
    
    * Fix response for collect
    
    * Add air to go server
    
    * Tidy go mod
    
    * add extra line

---
+++ server/internal/api/collect.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type collectRequest struct {
+	BoardID   boardID `json:"board_id" validate:"required"`
+	Spatials  [][]int `json:"spatials" validate:"required"`
+	Positions [][]int `json:"positions" validate:"required"`
+}
+
+func collect(c echo.Context) error {
+	var cr collectRequest
+
+	err := c.Bind(&cr)
+	if err != nil {
+		c.Logger().Errorf("Invalid collect request body: %s", err)
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid request body")
+	}
+
+	err = c.Validate(&cr)
+	if err != nil {
+		return err
+	}
+
+	_, ok := boards[cr.BoardID]
+	if !ok {
+		boards[cr.BoardID] = &positionData{
+			make([][]int, 0),
+			make([][]int, 0),
+		}
+	}
+
+	points := boards[cr.BoardID]
+	points.spatialPoints = append(points.spatialPoints, cr.Spatials...)
+	points.positionPoints = append(points.positionPoints, cr.Positions...)
+
+	c.Logger().Printf("Spatial Points: ", points.spatialPoints)
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"msg": "succesfully merged point data",
+	})
+}
